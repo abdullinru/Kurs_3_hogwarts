96dc3f7395274a3a06e753a51921f2e0e2d8497a
abdullinru <abdullin17021989@gmail.com>
2022-09-18 20:48:26 +0600

    контроллер, сервис и репозиторий для Avatar

---
+++ src/main/java/ru/hogwarts/school/controller/AvatarController.java
@@ -0,0 +1,8 @@
+package ru.hogwarts.school.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AvatarController {
+
+}
---
+++ src/main/java/ru/hogwarts/school/service/AvatarService.java
@@ -0,0 +1,12 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class AvatarService {
+
+
+}
