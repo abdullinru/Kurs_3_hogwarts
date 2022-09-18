e3069a96d498679f8ae7b6a7172a188acc67a63e
abdullinru <abdullin17021989@gmail.com>
2022-09-18 23:38:29 +0600

    конст-р в модель и методы equals и hashcode

--- src/main/java/ru/hogwarts/school/model/Avatar.java
+++ src/main/java/ru/hogwarts/school/model/Avatar.java
@@ -1,6 +1,8 @@
 package ru.hogwarts.school.model;
 
 import javax.persistence.*;
+import java.util.Arrays;
+import java.util.Objects;
 
 @Entity
 public class Avatar {
@@ -15,6 +17,44 @@ public class Avatar {
 
     @OneToOne
     private Student student;
+
+    public Avatar() {
+
+    }
+    public Avatar(Long id, String filePath, long fileSize, String mediaType, byte[] data, Student student) {
+        this.id = id;
+        this.filePath = filePath;
+        this.fileSize = fileSize;
+        this.mediaType = mediaType;
+        this.data = data;
+        this.student = student;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Avatar avatar = (Avatar) o;
+
+        if (fileSize != avatar.fileSize) return false;
+        if (!Objects.equals(id, avatar.id)) return false;
+        if (!Objects.equals(filePath, avatar.filePath)) return false;
+        if (!Objects.equals(mediaType, avatar.mediaType)) return false;
+        if (!Arrays.equals(data, avatar.data)) return false;
+        return Objects.equals(student, avatar.student);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (filePath != null ? filePath.hashCode() : 0);
+        result = 31 * result + (int) (fileSize ^ (fileSize >>> 32));
+        result = 31 * result + (mediaType != null ? mediaType.hashCode() : 0);
+        result = 31 * result + Arrays.hashCode(data);
+        result = 31 * result + (student != null ? student.hashCode() : 0);
+        return result;
+    }
 
     public Long getId() {
         return id;
